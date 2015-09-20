1cc024a4cac506ccef6863b5fe0ab5509a8e694e
Ing. Richard J. David G <ing_richardavid@hotmail.com>
2015-09-20 13:59:52 -0430

    Se añadieron comentarios a la clase Start

--- src/com/electrical_installations/view/Start.java
+++ src/com/electrical_installations/view/Start.java
@@ -8,40 +8,28 @@ import javax.swing.JFrame;
 import org.jvnet.substance.SubstanceLookAndFeel;
 
 /**
- *
+ * Clase que encapsula el inicio de la aplicación.
  * @author Ing. Richard David
+ * @version 1
+ * @since 2015-09-20
  */
 public class Start {    
     
+    /**
+     * Método main para iniciar la aplicación.
+     * @param args 
+     */
     public static void main(String args[]) { 
+        
+        /**
+         * Configuración de la librería Substance para dar look a la aplicación. 
+         */
         JFrame.setDefaultLookAndFeelDecorated(true);
         SubstanceLookAndFeel.setSkin("org.jvnet.substance.skin.ModerateSkin");
         SubstanceLookAndFeel.setCurrentTheme("org.jvnet.substance.theme.SubstanceColorBlindTheme");
         SubstanceLookAndFeel.setCurrentWatermark("org.jvnet.substance.watermark.SubstanceCopperplateEngravingWatermark");
         ViewLogin viewLogin  = new ViewLogin();
         viewLogin.setVisible(true);
-
-//        try {
-//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-//                if ("Windows".equals(info.getName())) {
-//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-//                    break;
-//                }
-//            }
-//            java.awt.EventQueue.invokeLater(new Runnable() {
-//                public void run() {
-//                    new ViewLogin().setVisible(true);
-//                }
-//            });
-//        } catch (ClassNotFoundException ex) {
-//            java.util.logging.Logger.getLogger(ViewUser.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (InstantiationException ex) {
-//            java.util.logging.Logger.getLogger(ViewUser.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (IllegalAccessException ex) {
-//            java.util.logging.Logger.getLogger(ViewUser.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-//            java.util.logging.Logger.getLogger(ViewUser.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        }
         
     }
     
